f252b57a0e983f58c1ea65668df841254bae1a1a
KakoF <kakoferrare87@gmail.com>
2019-05-28 19:26:42 -0300

    Correção

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            teste: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
