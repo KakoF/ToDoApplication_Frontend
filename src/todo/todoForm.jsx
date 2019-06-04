c3430e45cef58825990b60c0e0de15e27701784c
KakoF <kakoferrare87@gmail.com>
2019-06-04 20:29:06 -0300

    Eventos e ajustes no this

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -4,10 +4,14 @@ import Button from '../template/iconButton'
 export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
+            <input id='description' className='form-control' 
+                placeholder='Adicione uma tarefa' 
+                value={props.description}
+                onChange={props.handleChange}>
+            </input>
         </Grid>
         <Grid cols='12 3 2'>
-            <Button style='primary' icon='plus'></Button>
+            <Button style='primary' icon='plus' onClick={props.handleAdd}></Button>
         </Grid>
     </div>
 )
